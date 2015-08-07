e450f2f7d9a06f8c47f7d456e92c407448bfab68
jupiter <pieter@wavana.com>
2015-08-07 15:59:03 +0100

    Added routes provider and agent
    Fix for routes not updating

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ var msb = require('msb');
 var app = exports;
 
 app.config = require('./lib/config');
-app.router = require('./lib/router');
-app.server = require('./lib/server');
 
 app.start = function(cb) {
+  var RouterWrapper = require('./lib/routerWrapper').RouterWrapper;
+  app.router = new RouterWrapper();
+
   app.router.load(app.config.routes);
 
-  app.server
+  app.createServer()
   .listen(app.config.port)
   .once('listening', function() {
     app.config.port = this.address().port;
@@ -17,4 +18,20 @@ app.start = function(cb) {
     if (cb) { cb(); }
     console.log('http2bus listening on ' + app.config.port);
   });
+};
+
+app.createServer = function() {
+  var http = require('http');
+  var finalhandler = require('finalhandler');
+
+  return http.createServer(function(req, res) {
+    app.router.middleware(req, res, finalhandler(req, res));
+  });
+};
+
+app.createRoutesProviderAgent = function(config) {
+  return require('./lib/routesProvider/agent')
+    .create(config)
+    .load(config.routes)
+    .start();
 };
